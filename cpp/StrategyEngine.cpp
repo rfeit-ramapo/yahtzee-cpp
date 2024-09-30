8470ffcdccf402d3adf57bdc59238d43670fe5ac
rfeit <rfeit@ramapo.edu>
2024-09-29 22:00:46 -0400

    Document StrategyEngine class

--- cpp/StrategyEngine.cpp
+++ cpp/StrategyEngine.cpp
@@ -1,15 +1,29 @@
 #include "../include/StrategyEngine.h"
 
+/* *********************************************************************
+Function Name: GetPossibleCategories
+Purpose: Checks all categories that can be achieved, given locked dice
+Parameters: 
+            a_dice, a pointer to const Dice object to compare against
+Return Value: a vector of category indices that are possible for this dice set
+Algorithm:
+    1) Gets a reference to every category
+    2) For each category, gets a reroll strategy
+        3) If the strategy is worth 0 max points, it is impossible to achieve
+        4) If possible, add the category index to a vector
+    5) Return the vector of possible category indices
+Reference: none
+********************************************************************* */
 vector<int> StrategyEngine::GetPossibleCategories(shared_ptr<const Dice> a_dice) const
 {
-    // Set up the boolean vector to hold whether each category is available.
+    // Set up the vector to hold category indices.
     vector<int> possibleCategories;
 
     // Retrieve the categories from the scorecard
     const vector<shared_ptr<Category>>& categories = m_scorecard->GetCategories();
 
     // Check if max possible score > 0 (if the category is possible given current diceset)
-    for (int i = 0; i < categories.size(); ++i)
+    for (int i = 0; i < Scorecard::NUM_CATEGORIES; ++i)
     {
         if (categories[i]->GetRerollStrategy(a_dice).GetMaxScore())
         {
@@ -21,12 +35,26 @@ vector<int> StrategyEngine::GetPossibleCategories(shared_ptr<const Dice> a_dice)
     return possibleCategories;
 }
 
+/* *********************************************************************
+Function Name: Strategize
+Purpose: Determines the best strategy to follow based on the given dice set
+Parameters: 
+            a_dice, a pointer to const Dice object to analyze
+Return Value: the best Strategy found
+Algorithm:
+    1) Get a reference to every category
+    2) For each category, check the reroll strategy for this dice set
+        3) If the strategy is greater than the previous best strategy, replace it
+    4) Return the final best strategy found
+Reference: none
+********************************************************************* */
 Strategy StrategyEngine::Strategize(shared_ptr<const Dice> a_dice) const
 {
     // Retrieve the categories from the scorecard
     const vector<shared_ptr<Category>>& categories = m_scorecard->GetCategories();
 
-    Strategy strategy; // Use default constructor to initialize
+    // Use default constructor to initialize
+    Strategy strategy; 
 
     // Iterate over each category
     for (const auto& category : categories)
--- include/StrategyEngine.h
+++ include/StrategyEngine.h
@@ -6,22 +6,54 @@
 
 using namespace std;
 
+// Class to generate strategies.
 class StrategyEngine
 {
-    public:
+public:
+    // Constructors
 
-        vector<int> GetPossibleCategories(shared_ptr<const Dice> a_dice) const;
+    // Default
+    StrategyEngine() {};
+    // Including scorecard pointer
+    StrategyEngine(shared_ptr<Scorecard> a_scorecard) : m_scorecard(a_scorecard) {};
 
-        Strategy Strategize(shared_ptr<const Dice> a_dice) const;
+    // Selectors
+    shared_ptr<const Scorecard> GetScorecard() const { return m_scorecard; }
 
-        // Constructors
-        StrategyEngine() {}; // Default
+    // Functions
 
-        StrategyEngine(shared_ptr<Scorecard> a_scorecard) : m_scorecard(a_scorecard) {};
+    /* *********************************************************************
+    Function Name: GetPossibleCategories
+    Purpose: Checks all categories that can be achieved, given locked dice
+    Parameters: 
+                a_dice, a pointer to const Dice object to compare against
+    Return Value: a vector of category indices that are possible for this dice set
+    Algorithm:
+        1) Gets a reference to every category
+        2) For each category, gets a reroll strategy
+            3) If the strategy is worth 0 max points, it is impossible to achieve
+            4) If possible, add the category index to a vector
+        5) Return the vector of possible category indices
+    Reference: none
+    ********************************************************************* */
+    vector<int> GetPossibleCategories(shared_ptr<const Dice> a_dice) const;
 
-        // Getters
-        shared_ptr<const Scorecard> GetScorecard() const { return m_scorecard; }
+    /* *********************************************************************
+    Function Name: Strategize
+    Purpose: Determines the best strategy to follow based on the given dice set
+    Parameters: 
+                a_dice, a pointer to const Dice object to analyze
+    Return Value: the best Strategy found
+    Algorithm:
+        1) Get a reference to every category
+        2) For each category, check the reroll strategy for this dice set
+            3) If the strategy is greater than the previous best strategy, replace it
+        4) Return the final best strategy found
+    Reference: none
+    ********************************************************************* */
+    Strategy Strategize(shared_ptr<const Dice> a_dice) const;
 
-    private:
-        shared_ptr<Scorecard> m_scorecard;
+private:
+    // Pointer to the scorecard for this game
+    shared_ptr<Scorecard> m_scorecard;
 };
