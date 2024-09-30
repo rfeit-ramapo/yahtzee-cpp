d923b28bcd135cd0a04f43df9d8f40dbc73da878
rfeit <rfeit@ramapo.edu>
2024-09-29 20:13:11 -0400

    Document Dice class

--- cpp/Dice.cpp
+++ cpp/Dice.cpp
@@ -2,10 +2,21 @@
 #include <iostream>
 #include "../include/Dice.h"
 
-
+/* *********************************************************************
+Function Name: ListToCount
+Purpose: Turn a list of dice into a vector of dice face counts
+Parameters: 
+            a_diceList, the list of dice by face.
+Return Value: a vector representing how many of each face the dice list contained.
+Algorithm:
+    1) Loops through the dice list
+        2) For each face value, increment corresponding index of the diceCount vector
+    3) Return the diceCount vector
+Reference: none
+********************************************************************* */
 vector<int> Dice::ListToCount(vector<int> a_diceList)
 {
-    vector<int> diceCount(6,0);
+    vector<int> diceCount(NUM_DICE_FACES,0);
     for (int i = 0; i < a_diceList.size(); ++i)
     {
         ++diceCount[a_diceList[i] - 1];
@@ -13,10 +24,22 @@ vector<int> Dice::ListToCount(vector<int> a_diceList)
     return diceCount;
 }
 
+/* *********************************************************************
+Function Name: CountToList
+Purpose: Turn a vector of dice face counts into a list of dice faces
+Parameters: 
+            a_diceCount, the vector of dice face counts.
+Return Value: a vector of integers [1-6] that represent dice faces
+Algorithm:
+    1) Loops through the dice counts vector
+        2) For each additional die, add the face value to the dice list
+    3) Return the diceList vector
+Reference: none
+********************************************************************* */
 vector<int> Dice::CountToList(vector<int> a_diceCount)
 {
     vector<int> diceList;
-    for (int i = 0; i < 6; ++i)
+    for (int i = 0; i < NUM_DICE_FACES; ++i)
     {
         for (int j = 0; j < a_diceCount[i]; ++j)
         {
@@ -26,12 +49,29 @@ vector<int> Dice::CountToList(vector<int> a_diceCount)
     return diceList;
 }
 
+/* *********************************************************************
+Function Name: LockDice
+Purpose: Lock all dice that are not designated to keep free
+Parameters: 
+            a_keptDice, the vector of dice face counts to keep free.
+Return Value: a boolean indicating if locking was successful
+Algorithm:
+    1) Loops through the dice face
+        2) Try to lock any dice that are not kept
+        3) If trying to unlock dice, report error and return
+    4) Set the locked dice and return successfully
+Reference: none
+********************************************************************* */
 bool Dice::LockDice(vector<int> a_keptDice)
 {
-    vector<int> newLocked(6,0);
-    for (int i = 0; i < 6; ++i)
+    // Loop through each dice face
+    vector<int> newLocked(NUM_DICE_FACES,0);
+    for (int i = 0; i < NUM_DICE_FACES; ++i)
     {
+        // Lock any dice that are not specified as kept.
         int numToLock = m_diceCount[i] - a_keptDice[i];
+        // If the new number to lock is less than before, report error.
+        // You cannot unlock dice with this function.
         if (numToLock < m_locked[i])
         {
             cerr << "Error: Input must be dice that were rolled and that have not already been set aside. Please try again." << endl;
@@ -39,87 +79,148 @@ bool Dice::LockDice(vector<int> a_keptDice)
         }
         newLocked[i] = numToLock;
     }
+
+    // If there were no errors, set the locked dice and return true (indicating success)
     m_locked = newLocked;
     return true;
 }
 
+/* *********************************************************************
+Function Name: RollAll
+Purpose: Rolls all dice that are not locked.
+Parameters: None
+Return Value: a vector indicating what the new faces are.
+Algorithm:
+    1) Loops through the die
+        2) Rerolls the die if it is not locked
+        3) If it is locked, skips and reduces locked count for that face
+    2) Returns the new dice list
+Reference: none
+********************************************************************* */
 vector<int> Dice::RollAll()
 {
+    // Keep track of how many locked dice remain
     m_diceCount = m_locked;
-    vector<int> lockedRemaining = m_locked; // keep track of how many locked dice remain
-    for (int i = 0; i < 5; ++i) 
+    vector<int> lockedRemaining = m_locked;
+
+    // Loop through each die
+    for (int i = 0; i < NUM_DICE; ++i) 
     {
+        // If this face value is locked, do not reroll it.
         if (lockedRemaining[(m_diceList[i] - 1)])
         {
+            // Decrement locked so that any extra of this face can be rerolled.
             --lockedRemaining[(m_diceList[i] - 1)];
             continue;
         }
+
+        // Replace this die with a newly generated value.
         m_diceList[i] = GenerateDieValue();
+        // Update the dice face counts as well.
         m_diceCount[(m_diceList[i] - 1)] += 1;
     }
     return m_diceList;
 }
 
-bool Dice::ManualRoll(vector<int> a_input)
+/* *********************************************************************
+Function Name: ManualRoll
+Purpose: Allows for manual input of reroll values for free dice
+Parameters: 
+            a_input, a vector of integers representing the rerolled dice
+Return Value: None
+Algorithm:
+    1) Uses locked dice as the base
+    2) Adds inputted dice to dice list
+    3) Increments face counts based on inputted dice
+Reference: none
+********************************************************************* */
+void Dice::ManualRoll(vector<int> a_input)
 {
-    // Make the dice match
+    // Set locked dice as base
     m_diceCount = m_locked;
     m_diceList = CountToList(m_locked);
+
+    // Append input list to locked values
     m_diceList.insert(m_diceList.end(), a_input.begin(), a_input.end());
     
+    // Add to dice counts for every input face value
     for (int i = 0; i < a_input.size(); ++i)
     {
         ++m_diceCount[m_diceList[i] - 1];
     }
-
-    return true;
 }
 
-vector<int> Dice::RerollToMatch(const vector<int>& target) const
-{
-    vector<int> rerollValues = {0, 0, 0, 0, 0, 0};
-    for (int i = 0; i < m_diceCount.size(); ++i)
-    {
-        // Return negative values if this is impossible to achieve
-        if (m_locked[i] > target[i]) return {-1, -1, -1, -1, -1, -1};
-        rerollValues[i] = max(m_diceCount[i] - target[i], 0);
-    }
-    return rerollValues;
-}
 
+/* *********************************************************************
+Function Name: GetUnlockedUnscored
+Purpose: To get the counts of dice of each face that are not contributing to a score
+Parameters: 
+            a_required, a constant reference that represents which dice faces contribute to a score.
+Return Value: a vector of integers that represent how many of each dice face do not contribute.
+Algorithm:
+    1) Loops through each dice face and calculates free dice that are not required
+    2) Returns the resulting vector
+Reference: none
+********************************************************************* */
 vector<int> Dice::GetUnlockedUnscored(const vector<int>& required) const
 {
-    vector<int> unlockedUnscored = {0, 0, 0, 0, 0, 0};
-    for (int i = 0; i < 6; ++i)
+    vector<int> unlockedUnscored(NUM_DICE_FACES, 0);
+    for (int i = 0; i < NUM_DICE_FACES; ++i)
     {
+        // Get whichever is greater: required dice of this face, or locked dice of this face
+        // Subtract the above from current dice to see if there are extraneous dice of this face
+        // Take the max between this and zero to avoid negative numbers
         unlockedUnscored[i] = max(m_diceCount[i] - max(required[i], m_locked[i]), 0);
     }
     return unlockedUnscored;
 }
 
+/* *********************************************************************
+Function Name: GetFreeDice
+Purpose: To get counts of unlocked dice faces
+Parameters: None
+Return Value: a vector of integers that represent how many of each dice face are not locked.
+Algorithm:
+    1) Loops through each dice face and calculates total count - locked count
+    2) Returns the resulting vector
+Reference: none
+********************************************************************* */
 vector<int> Dice::GetFreeDice() const
 {
     vector<int> freeDice;
-    for (int i = 0; i < 6; ++i)
+    for (int i = 0; i < NUM_DICE_FACES; ++i)
     {
         freeDice.push_back(m_diceCount[i] - m_locked[i]);
     }
     return freeDice;
 }
 
+/* *********************************************************************
+Function Name: Print
+Purpose: Prints out the current list of dice, with locked dice in red.
+Parameters: None
+Return Value: None
+Algorithm:
+    1) Loops through the dice list and prints out each face value
+        2) If this face is locked, it prints in red
+Reference: none
+********************************************************************* */
 void Dice::Print() const
 {
-    #define RESET   "\033[0m"
-    #define RED     "\033[31m"
+    const string RESET_COLOR = "\033[0m";
+    const string RED_COLOR = "\033[31m";
 
-    vector<int> lockedRemaining = m_locked; // keep track of how many locked dice remain
-    for (int i = 0; i < 5; ++i) 
+    // Keep track of how many locked dice remain
+    vector<int> lockedRemaining = m_locked; 
+    for (int i = 0; i < NUM_DICE; ++i) 
     {
+        // Print in red if this die is locked
         if (lockedRemaining[(m_diceList[i] - 1)])
         {
             --lockedRemaining[(m_diceList[i] - 1)];
-            cout << RED << m_diceList[i] << RESET << " ";
+            cout << RED_COLOR << m_diceList[i] << RESET_COLOR << " ";
         }
+        // Otherwise print normally
         else
         {
             cout << m_diceList[i] << " ";
@@ -128,6 +229,16 @@ void Dice::Print() const
     cout << endl;
 }
 
+/* *********************************************************************
+Function Name: GenerateDieValue
+Purpose: Randomly generates a number between 1 and 6
+Parameters: None
+Return Value: an integer between 1 and 6, representing a die face
+Algorithm:
+    1) Create and seed a random generator
+    2) Use uniform distribution to generate a value between 1 and 6
+Reference: Used ChatGPT to research random generators
+********************************************************************* */
 int Dice::GenerateDieValue()
 {
     random_device rd;
--- include/Dice.h
+++ include/Dice.h
@@ -6,56 +6,183 @@ using namespace std;
 
 class Dice
 {
-    public:
+public:
+    // Constants
 
-        static constexpr int NUM_DICE = 5;
-        static constexpr int NUM_DICE_FACES = 6;
+    // Number of dice rolled for the game
+    static constexpr int NUM_DICE = 5;
+    // The number of dice faces
+    static constexpr int NUM_DICE_FACES = 6;
 
-        inline int RollOne() { return GenerateDieValue(); }
-        inline void LockAllDice() { m_locked = m_diceCount; }
-        inline void UnlockAllDice() { m_locked = {0, 0, 0, 0, 0, 0}; }
+    // Constructor
+    Dice() {};
 
-        static vector<int> ListToCount(vector<int> a_diceList);
-        static vector<int> CountToList(vector<int> a_diceCount);
-        
-        // Lock all dice except those specified to reroll.
-        bool LockDice(vector<int> a_keptDice);
+    // Selectors
+    inline vector<int> GetLockedDice() const { return m_locked; }
+    inline vector<int> GetDiceCount() const { return m_diceCount; }
 
-        vector<int> RollAll();
-        bool ManualRoll(vector<int> a_input);
+    // Inline Functions
 
-        // Returns a vector indicating which dice values to reroll to match the parameter.
-        vector<int> RerollToMatch(const vector<int>& target) const;
+    /* *********************************************************************
+    Function Name: RollOne
+    Purpose: To roll one die and return the value
+    Parameters: None
+    Return Value: an integer [1,6] representing the face value rolled
+    Algorithm:
+        1) Return the result of GenerateDieValue
+    Reference: none
+    ********************************************************************* */
+    inline int RollOne() { return GenerateDieValue(); }
 
-        // Find unlocked, non-scoring dice
-        // required paramter represents dice that would contribute to the score
-        vector<int> GetUnlockedUnscored(const vector<int>& required) const;
+    /* *********************************************************************
+    Function Name: LockAllDice
+    Purpose: Locks all dice to prevent rerolling them
+    Parameters: None
+    Return Value: None
+    Algorithm:
+        1) Set locked dice equal to the current dice face counts
+    Reference: none
+    ********************************************************************* */
+    inline void LockAllDice() { m_locked = m_diceCount; }
 
-        // Returns the number of dice of each face value that are NOT locked
-        vector<int> GetFreeDice() const;
+    /* *********************************************************************
+    Function Name: UnlockAllDice
+    Purpose: Unlocks all dice to allow for rerolls
+    Parameters: None
+    Return Value: None
+    Algorithm:
+        1) Clears the locked dice vector to all 0s
+    Reference: none
+    ********************************************************************* */
+    inline void UnlockAllDice() { m_locked = {0, 0, 0, 0, 0, 0}; }
 
-        void Print() const;
+    /* *********************************************************************
+    Function Name: ListToCount
+    Purpose: Turn a list of dice into a vector of dice face counts
+    Parameters: 
+                a_diceList, the list of dice by face.
+    Return Value: a vector representing how many of each face the dice list contained.
+    Algorithm:
+        1) Loops through the dice list
+            2) For each face value, increment corresponding index of the diceCount vector
+        3) Return the diceCount vector
+    Reference: none
+    ********************************************************************* */
+    static vector<int> ListToCount(vector<int> a_diceList);
 
-        // Getters
-        inline vector<int> GetLockedDice() const { return m_locked; }
-        inline vector<int> GetDiceCount() const { return m_diceCount; }
+    /* *********************************************************************
+    Function Name: CountToList
+    Purpose: Turn a vector of dice face counts into a list of dice faces
+    Parameters: 
+                a_diceCount, the vector of dice face counts.
+    Return Value: a vector of integers [1-6] that represent dice faces
+    Algorithm:
+        1) Loops through the dice counts vector
+            2) For each additional die, add the face value to the dice list
+        3) Return the diceList vector
+    Reference: none
+    ********************************************************************* */
+    static vector<int> CountToList(vector<int> a_diceCount);
+    
+    /* *********************************************************************
+    Function Name: LockDice
+    Purpose: Lock all dice that are not designated to keep free
+    Parameters: 
+                a_keptDice, the vector of dice face counts to keep free.
+    Return Value: a boolean indicating if locking was successful
+    Algorithm:
+        1) Loops through the dice face
+            2) Try to lock any dice that are not kept
+            3) If trying to unlock dice, report error and return
+        4) Set the locked dice and return successfully
+    Reference: none
+    ********************************************************************* */
+    bool LockDice(vector<int> a_keptDice);
 
-        // Constructors
-        Dice() {}; // Default
+    /* *********************************************************************
+    Function Name: RollAll
+    Purpose: Rolls all dice that are not locked.
+    Parameters: None
+    Return Value: a vector indicating what the new faces are.
+    Algorithm:
+        1) Loops through the die
+            2) Rerolls the die if it is not locked
+            3) If it is locked, skips and reduces locked count for that face
+        2) Returns the new dice list
+    Reference: none
+    ********************************************************************* */
+    vector<int> RollAll();
 
-        Dice(vector<int> a_diceCount) : m_diceCount(a_diceCount) {};
+    /* *********************************************************************
+    Function Name: ManualRoll
+    Purpose: Allows for manual input of reroll values for free dice
+    Parameters: 
+                a_input, a vector of integers representing the rerolled dice
+    Return Value: None
+    Algorithm:
+        1) Uses locked dice as the base
+        2) Adds inputted dice to dice list
+        3) Increments face counts based on inputted dice
+    Reference: none
+    ********************************************************************* */
+    void ManualRoll(vector<int> a_input);
 
-        Dice(
-            vector<int> a_diceCount, 
-            vector<int> a_locked
-        ) : m_diceCount(a_diceCount), m_locked(a_locked) 
-        {};
 
-    private:
-        int GenerateDieValue();
 
-        vector<int> m_diceList = {1, 1, 1, 1, 1};
-        vector<int> m_diceCount = {0, 0, 0, 0, 0, 0};
-        vector<int> m_locked = {0, 0, 0, 0, 0, 0};
+    /* *********************************************************************
+    Function Name: GetUnlockedUnscored
+    Purpose: To get the counts of dice of each face that are not contributing to a score
+    Parameters: 
+                a_required, a constant reference that represents which dice faces contribute to a score.
+    Return Value: a vector of integers that represent how many of each dice face do not contribute.
+    Algorithm:
+        1) Loops through each dice face and calculates free dice that are not required
+        2) Returns the resulting vector
+    Reference: none
+    ********************************************************************* */
+    vector<int> GetUnlockedUnscored(const vector<int>& required) const;
 
+    /* *********************************************************************
+    Function Name: GetFreeDice
+    Purpose: To get counts of unlocked dice faces
+    Parameters: None
+    Return Value: a vector of integers that represent how many of each dice face are not locked.
+    Algorithm:
+        1) Loops through each dice face and calculates total count - locked count
+        2) Returns the resulting vector
+    Reference: none
+    ********************************************************************* */
+    vector<int> GetFreeDice() const;
+
+    /* *********************************************************************
+    Function Name: Print
+    Purpose: Prints out the current list of dice, with locked dice in red.
+    Parameters: None
+    Return Value: None
+    Algorithm:
+        1) Loops through the dice list and prints out each face value
+            2) If this face is locked, it prints in red
+    Reference: none
+    ********************************************************************* */
+    void Print() const;
+
+private:
+    // A vector containing the face values of all dice
+    vector<int> m_diceList = {1, 1, 1, 1, 1};
+    // A vector containing the counts of each dice face [1-6]
+    vector<int> m_diceCount = {0, 0, 0, 0, 0, 0};
+    // A vector containing the counts of each locked dice face [1-6]
+    vector<int> m_locked = {0, 0, 0, 0, 0, 0};
+
+    /* *********************************************************************
+    Function Name: GenerateDieValue
+    Purpose: Randomly generates a number between 1 and 6
+    Parameters: None
+    Return Value: an integer between 1 and 6, representing a die face
+    Algorithm:
+        1) Create and seed a random generator
+        2) Use uniform distribution to generate a value between 1 and 6
+    Reference: Used ChatGPT to research random generators
+    ********************************************************************* */
+    int GenerateDieValue();
 };
